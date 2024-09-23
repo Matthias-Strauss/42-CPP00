546ccd2b44c212b73bcfc50087207327dfa5abb0
mstrauss <matthias.strauss@proton.me>
2024-09-23 13:44:12 +0200

    removed .hpp, unneccessary for this exercise

--- ex00/srcs/megaphone.cpp
+++ ex00/srcs/megaphone.cpp
@@ -6,13 +6,12 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 15:06:25 by mstrauss          #+#    #+#             */
-/*   Updated: 2024/09/19 19:54:44 by mstrauss         ###   ########.fr       */
+/*   Updated: 2024/09/20 13:08:00 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "megaphone.hpp"
-
-//
+#include <iostream>
+#include <string>
 
 int main(int ac, char **av)
 {
